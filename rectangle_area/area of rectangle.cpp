336bae12b2eff7a2cb55391b0f12e664aace7b11
Abdii345 <heykalyare4@gmail.com>
2024-09-16 04:18:11 -0700

    area of rectangle

---
+++ rectangle_area/area of rectangle.cpp
@@ -0,0 +1,13 @@
+1. #include<stdio.h>
+2. int main(){
+3.	foat length,width,area;
+4.	printf("enter length of rectangle/n");
+5.	scanf("%f",&length);
+	
+6.	printf("enter width of rectangle/n");
+7.	scanf("%f",&width);
+	
+7.	area=length*width;
+8		printf("area of rectangle is &f/n",area);
+	return0
+}
